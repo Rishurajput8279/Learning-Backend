613bba5901692bd4393504c826796725d178591a
Reshu Jaynt <reshu.22b1531032@abes.ac.in>
2025-09-03 23:54:24 +0530

    firstCode

---
+++ index.js
@@ -0,0 +1,18 @@
+// import express from "express";
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send("RishuRajput");
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please Login in Chai Aur Code</h1>');
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
